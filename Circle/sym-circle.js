859fb70bab5416b056161e36d8f36302266be8ee
Jenny <jennifer.castillo@usm.cl>
2024-02-07 12:25:42 -0300

    Underline funcionando, falta Text Size

--- Circle/sym-circle.js
+++ Circle/sym-circle.js
@@ -6,8 +6,6 @@
 	symbolVis.prototype.init = function (scope, elem) {
         //Funcion que se llama cada vez que hay un cambio en la data
         this.onDataUpdate = dataUpdate;
-        this.OnConfigChange = customConfig;
-        
 
         //Funciona solo cuando se updatea la data
         function dataUpdate(data) {
@@ -18,35 +16,7 @@
             //Variable que contiene solo el tiempo y la envia al dom
             scope.time = now.toLocaleTimeString();
 
-            //Variable que encuentra el texto en el dom
-            var text1 = elem.find('#texto1')[0];
-            var text2 = elem.find('#texto2')[0];
 
-            //variable que busca el div
-            // var div =  elem.find('#container')[0];
-
-            //Cambio de tamaño del texto
-            text1.style.fontSize = scope.config.FontSize + 'px';
-            text2.style.fontSize = scope.config.FontSize + 'px';
-
-            //Underline
-            //Si es que ShowUnderlines es true
-            if(scope.config.ShowUnderline){
-                //Se cambia el estilo
-                text1.style.textDecoration = 'underline';
-                
-                text2.style.textDecoration = 'underline';
-            }
-            else{
-                text1.style.textDecoration = 'none';
-                
-                text2.style.textDecoration = 'none';
-            }
-        }
-    
-        function customConfig(newConfig, oldConfig){
-            oldConfig = getDefaultConfig;
-            
         }
     };
 
